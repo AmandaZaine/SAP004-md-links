5666d590271c312cea1180e0b64b70d234bb5d1d
Zaine Nascimento <amandazaine@outlook.com>
2020-07-09 01:53:49 -0300

    Corringindo RegExQuePegaOsLinksEaDescricaoDoLink para pegar todos os links do README.md

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ function mdLinks(path){
       console.log(err)
     }else{
       const RegExQuePegaOsLinks = /\((http[s]?:.*)\)/gm;
-      const RegExQuePegaOsLinksEaDescricaoDoLink = /\[(\S.*)\]\((http[s]?:.*)\)/gm;
+      //const RegExQuePegaOsLinksEaDescricaoDoLink = /\[(\S.*)\]\((http[s]?:.*)\)/gm;
+      //const RegExQuePegaOsLinksEaDescricaoDoLink = /\[([^\[]*)\]\((http[s]?:[A-Za-z0-9,-_#.]*)\)/gm;
+      const RegExQuePegaOsLinksEaDescricaoDoLink = /\[([^\[]*)\]\((http[s]?:[A-Za-z0-9,-_#.@%]*)\)/gm;
+      
       //Comparar o RegEx com o texto do arquivo
       //Pegar os links
       const arrayComTodosOsLinks = data.match(RegExQuePegaOsLinks)
@@ -27,17 +30,21 @@ function mdLinks(path){
         
         const quebrarTexto = descELink.split('](')
         const descricaoDoLink = quebrarTexto[0].replace(/\[/, "")
+        const descricaoDoLink2 = quebrarTexto[0]
         const linkLimpo = quebrarTexto[1].replace(/\)/g, "")
 
         //console.log(descricaoDoLink)
+        //console.log(typeof descricaoDoLink2)
         //console.log(linkLimpo)
+        //\\r && \\n
 
-        let objectLink = { href: `${linkLimpo}`, text: `${descricaoDoLink}` }
+        let objectLink = { href: `${linkLimpo}`, text: `${descricaoDoLink}`, file: `${path}` }
         
         arrayDeObjetosDosLinks.push(objectLink)
 
       }
 
+      //console.log(typeof arrayDeObjetosDosLinks[46])
       console.log(arrayDeObjetosDosLinks)
     }
   }) 
