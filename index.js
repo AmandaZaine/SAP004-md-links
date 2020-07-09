061201629f152d64631dfd0126e0801bb3125969
Zaine Nascimento <amandazaine@outlook.com>
2020-07-08 23:20:37 -0300

    Criando função/Lendo arquivo/Encontrando Links/Colocando Links num array de objetos

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-module.exports = () => {
+/*
+module.exports = (path, options) => {
   // ...
 };
+*/
+
+const fs = require("fs")
+function mdLinks(path){
+
+  fs.readFile(path, "utf8", (err, data) => {
+    if(err){
+      console.log(err)
+    }else{
+      const RegExQuePegaOsLinks = /\((http[s]?:.*)\)/gm;
+      const RegExQuePegaOsLinksEaDescricaoDoLink = /\[(\S.*)\]\((http[s]?:.*)\)/gm;
+      //Comparar o RegEx com o texto do arquivo
+      //Pegar os links
+      const arrayComTodosOsLinks = data.match(RegExQuePegaOsLinks)
+      const arrayComTodosOsLinksEDescricaoDeCadaLink = data.match(RegExQuePegaOsLinksEaDescricaoDoLink)
+      
+      //console.log(arrayComTodosOsLinksEDescricaoDeCadaLink)
+
+      const arrayDeObjetosDosLinks = []
+
+      for(let descELink of arrayComTodosOsLinksEDescricaoDeCadaLink){
+        //console.log(descELink)  
+        
+        const quebrarTexto = descELink.split('](')
+        const descricaoDoLink = quebrarTexto[0].replace(/\[/, "")
+        const linkLimpo = quebrarTexto[1].replace(/\)/g, "")
+
+        //console.log(descricaoDoLink)
+        //console.log(linkLimpo)
+
+        let objectLink = { href: `${linkLimpo}`, text: `${descricaoDoLink}` }
+        
+        arrayDeObjetosDosLinks.push(objectLink)
+
+      }
+
+      console.log(arrayDeObjetosDosLinks)
+    }
+  }) 
+}
+
+mdLinks("README.md")
