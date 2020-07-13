6026b6ef7ee90082101b59ddd5f1f785116ae4b7
Zaine Nascimento <amandazaine@outlook.com>
2020-07-13 00:00:44 -0300

    Adicionando --validate --stats

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function mdLinks(caminho, opcao){
                   arrayDeLinkEmObjetos[indice]["Status"] = statusDoLink.statusText;
                   arrayDeLinkEmObjetos[indice]["StatusAxios"] = statusDoLink.status;
                 }else{
-                  arrayDeLinkEmObjetos[indice]["Status"] = "Link Quebrado";
+                  arrayDeLinkEmObjetos[indice]["Status"] = "Fail";
                   arrayDeLinkEmObjetos[indice]["StatusAxios"] = statusDoLink.status;
                 }
               })
@@ -53,11 +53,81 @@ function mdLinks(caminho, opcao){
           })
         }
 
-        if(opcao.includes("--validate")){
+        function quantidadeDeLinksUnicos(linksEmObjetosParametro){
 
-        statusDoLink(linksEmObjetos).then((resultado) => {
-          return resolve(resultado);
-        })
+          const arrayDeLinks = [];
+
+            for(let link of linksEmObjetosParametro){
+              arrayDeLinks.push(link.href)
+            }
+
+            let linksUnicos = [];
+
+            for(let linkUnico of arrayDeLinks){
+              var indices = []
+              var array = arrayDeLinks
+              var elemento = linkUnico
+              var idx = array.indexOf(elemento)
+
+              while (idx != -1) {
+              indices.push(idx);
+              idx = array.indexOf(elemento, idx + 1);
+              }
+
+              if(indices.length === 1){
+                linksUnicos.push(linkUnico)
+              }else{
+
+                if(!linksUnicos.includes(linkUnico)){
+                  linksUnicos.push(linkUnico)
+                }
+              }
+            }
+
+            return (linksUnicos.length);
+
+        }
+
+        function quantidadeDeLinksQuebrados(resultadoParametro){
+            
+            const arrayDeStatusDosLinks = []
+
+            resultadoParametro.forEach((cadaLink) => {
+              //console.log(cadaLink.Status)
+              arrayDeStatusDosLinks.push(cadaLink.Status)
+            })
+
+            let linksQuebrados = 0;
+
+            for(let linkUnicoStatus of arrayDeStatusDosLinks){
+
+              if(linkUnicoStatus !== "OK"){
+                linksQuebrados++
+              }
+            }
+
+            return (linksQuebrados)
+        }
+
+        if(opcao.includes("--stats") && opcao.includes("--validate")){
+
+          statusDoLink(linksEmObjetos).then((resultado) => {
+
+            return resolve(`Total de Links: ${linksEmObjetos.length} | Quantidade de Links Unicos: ${quantidadeDeLinksUnicos(linksEmObjetos)} | Quantidade de Links Quebrados: ${quantidadeDeLinksQuebrados(resultado)}`);
+
+          })
+
+        }else if(opcao.includes("--validate")){
+
+          statusDoLink(linksEmObjetos).then((resultado) => {
+            return resolve(resultado);
+          })
+
+        }else if(opcao.includes("--stats")){
+
+          return resolve(
+            `Total de Links: ${linksEmObjetos.length} | Quantidade de Links Unicos: ${quantidadeDeLinksUnicos(linksEmObjetos)}`
+            )
 
         }else{
 
